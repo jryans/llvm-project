6a29c3a3a3d0920cbac2c351166bf0552c3ee912
J. Ryan Stinnett <jryans@gmail.com>
2026-03-03 12:32:35 +0000

    [clang][DebugInfo] Add inlined subprogram metadata for compiler built-ins
    This wraps compiler built-ins in an artificial inlined subprogram for
    debug info purposes. This makes compiler built-in usage legible to debug
    info consumers like profilers, which can then annotate time spent with
    the built-in function name.

--- clang/lib/CodeGen/CGDebugInfo.cpp
+++ clang/lib/CodeGen/CGDebugInfo.cpp
@@ -347,6 +347,19 @@ ApplyInlineDebugLocation::~ApplyInlineDebugLocation() {
   DI.EmitLocation(CGF->Builder, SavedLocation);
 }
 
+static llvm::DILocation *createBuiltinInlineAt(CodeGenFunction &CGF,
+                                               GlobalDecl GD) {
+  if (!CGF.getDebugInfo()) {
+    return nullptr;
+  }
+  auto &DI = *CGF.getDebugInfo();
+  return DI.createBuiltinFunctionLocation(CGF.Builder, GD);
+}
+
+ApplyBuiltinDebugLocation::ApplyBuiltinDebugLocation(CodeGenFunction &CGF,
+                                                     GlobalDecl BuiltinFn)
+    : CGF(&CGF), Apply(CGF, createBuiltinInlineAt(CGF, BuiltinFn)) {}
+
 void CGDebugInfo::setLocation(SourceLocation Loc) {
   // If the new location isn't valid return.
   if (Loc.isInvalid())
@@ -5025,6 +5038,14 @@ void CGDebugInfo::EmitInlineFunctionEnd(CGBuilderTy &Builder) {
   assert(CurInlinedAt && "unbalanced inline scope stack");
   EmitFunctionEnd(Builder, nullptr);
   setInlinedAt(llvm::DebugLoc(CurInlinedAt).getInlinedAt());
+}
+
+llvm::DILocation *
+CGDebugInfo::createBuiltinFunctionLocation(CGBuilderTy &Builder,
+                                           GlobalDecl GD) {
+  const auto *FD = cast<FunctionDecl>(GD.getDecl());
+  return CreateSyntheticInlineAt(Builder.getCurrentDebugLocation(),
+                                 getFunctionName(FD));
 }
 
 void CGDebugInfo::EmitLocation(CGBuilderTy &Builder, SourceLocation Loc) {
